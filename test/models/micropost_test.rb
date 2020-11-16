35c90e2653c55138d643202494bb6c08ed196afc
Darcey Lachtman <dslachtman@gmail.com>
2020-11-16 07:50:33 -1000

    add more tests that pass

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -17,12 +17,22 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
+  test "user id should be present again" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
   test "content should be present" do
     @micropost.content = "   "
     assert_not @micropost.valid?
   end
 
   test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
+   test "content should be at most 140 characters again" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
